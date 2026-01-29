4e82bacb8e59b0d9b68e867d8c5856c131d03509
Edwin-Gc <bytevisiondesign@gmail.com>
2026-01-29 08:54:13 -0500

    Fix pom: Java17, MySQL stable, Lombok fix

--- src/main/java/edw/Ops/modelo/Empleado.java
+++ src/main/java/edw/Ops/modelo/Empleado.java
@@ -1,93 +1,29 @@
 package edw.Ops.modelo;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Table(name = "empleado")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Empleado {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_empleado")
-    private Integer idEmpleado;
+    Integer idEmpleado;
+    String nombre;
+    String departamento;
+    String ciudad;
+    String telefono;
+    String email;
+    Double salario;
 
-    private String nombre;
-    private String departamento;
-    private String ciudad;
-    private String telefono;
-    private String email;
-    private Double salario;
-
-    // Constructor vacío
-    public Empleado() {}
-
-    // Constructor completo
-    public Empleado(Integer idEmpleado, String nombre, String departamento,
-                    String ciudad, String telefono, String email, Double salario) {
-        this.idEmpleado = idEmpleado;
-        this.nombre = nombre;
-        this.departamento = departamento;
-        this.ciudad = ciudad;
-        this.telefono = telefono;
-        this.email = email;
-        this.salario = salario;
-    }
-
-    // Getters y Setters
-
-    public Integer getIdEmpleado() {
-        return idEmpleado;
-    }
-
-    public void setIdEmpleado(Integer idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
-    }
 }
